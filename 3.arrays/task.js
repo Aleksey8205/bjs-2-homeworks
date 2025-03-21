a1a9faa3e25ac252dcf43076702b52b2adbfcdac
Alexey Danilov <Aleksey8205@yandex.ru>
2025-03-22 00:32:33 +0300

    решение задач с массивами

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,18 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+
+    return arr1.every((element, index) => element === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    if (users.length === 0 || (gender !== "мужской" && gender !== "женский")) {
+        return 0;
+    }
+
+    const filterUser = users.filter(user => user.gender === gender)
+    const totalAge = filterUser.reduce((sum, user) => sum + user.age, 0);
+    const averAge = totalAge / filterUser.length;
+    return averAge;
 }
